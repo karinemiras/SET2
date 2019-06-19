62c99337439bd5ed226a3a00717e221272620c0a
Karine <karine.smiras@gmail.com>
2019-06-19 14:08:53 +0200

    new evolution

--- set2_mlp_keras_cifar10.py
+++ set2_mlp_keras_cifar10.py
@@ -50,6 +50,10 @@ from sRelu import *
 from keras.datasets import cifar10
 from keras.utils import np_utils
 import argparse
+from random import randint
+import copy
+import datetime
+
 
 class Constraint(object):
 
@@ -58,6 +62,7 @@ class Constraint(object):
 
     def get_config(self):
         return {}
+
 
 class MaskWeights(Constraint):
 
@@ -73,28 +78,41 @@ class MaskWeights(Constraint):
         return {'mask': self.mask}
 
 
-def find_first_pos(array, value):
-    idx = (np.abs(array - value)).argmin()
-    return idx
+class Individual:
 
+    def __init__(self, wm1, wm2, wm3,
+                 mut_rate_wm1, mut_rate_wm2, mut_rate_wm3,
+                 w1 = None, w2 = None, w3 = None, w4 = None
+                 ):
+        self.wm1 = wm1
+        self.wm2 = wm2
+        self.wm3 = wm3
+        self.w1 = w1
+        self.w2 = w2
+        self.w3 = w3
+        self.w4 = w4
+        self.mut_rate_wm1 = mut_rate_wm1
+        self.mut_rate_wm2 = mut_rate_wm2
+        self.mut_rate_wm3 = mut_rate_wm3
+        self.model = None
+        self.fitness = None
 
-def find_last_pos(array, value):
-    idx = (np.abs(array - value))[::-1].argmin()
-    return array.shape[0] - idx
+    def mutate(self):
+        self.wm1 = self.mutation(self.wm1, self.mut_rate_wm1)
+        self.wm2 = self.mutation(self.wm2, self.mut_rate_wm2)
+        self.wm3 = self.mutation(self.wm3, self.mut_rate_wm3)
 
+    def mutation(self, wm, mut_rate):
+        try_mut = np.random.rand(len(wm), len(wm[0]))
+        flip = try_mut < mut_rate
+        wm_flipped = np.absolute((wm-1)*-1)
+        wm[flip] = wm_flipped[flip]
 
-def createWeightsMask(epsilon,noRows, noCols):
-    # generate an Erdos Renyi sparse weights mask
-    mask_weights = np.random.rand(noRows, noCols)
-    prob = 1 - (epsilon * (noRows + noCols)) / (noRows * noCols)  # normal tp have 8x connections
-    mask_weights[mask_weights < prob] = 0
-    mask_weights[mask_weights >= prob] = 1
-    noParameters = np.sum(mask_weights)
-    print ("Create Sparse Matrix: No parameters, NoRows, NoCols ",noParameters,noRows,noCols)
-    return [noParameters,mask_weights]
+        return wm
 
 
 class SET2_MLP_CIFAR10:
+
     def __init__(self, config):
         self.config = config
         # set model parameters
@@ -104,23 +122,23 @@ class SET2_MLP_CIFAR10:
         self.maxepoches = 1000 # number of epochs
         self.learning_rate = 0.01 # SGD learning rate
         self.num_classes = 10 # number of classes
-        self.momentum=0.9 # SGD momentum
+        self.momentum = 0.9 # SGD momentum
+        self.pop_size = 10 # number of individuals in the evolving population
+        self.epochs_per_generation = 20 # number of epochs to train solutions of a generation (naturally, number of generations is maxepoches/epochs_per_generation
 
         # generate an Erdos Renyi sparse weights mask for each layer
-        [self.noPar1, self.wm1] = createWeightsMask(self.epsilon,32 * 32 *3, 4000)
-        [self.noPar2, self.wm2] = createWeightsMask(self.epsilon,4000, 1000)
-        [self.noPar3, self.wm3] = createWeightsMask(self.epsilon,1000, 4000)
+        [self.noPar1, wm1, self.mut_rate_wm1] = self.createWeightsMask(self.epsilon, 32 * 32 * 3, 4000)
+        [self.noPar2, wm2, self.mut_rate_wm2] = self.createWeightsMask(self.epsilon, 4000, 1000)
+        [self.noPar3, wm3, self.mut_rate_wm3] = self.createWeightsMask(self.epsilon, 1000, 4000)
 
-        # initialize layers weights
-        self.w1 = None
-        self.w2 = None
-        self.w3 = None
-        self.w4 = None
+        # initialize masks replicating the initial sparse masks
+        self.population = [Individual(wm1.copy(), wm2.copy(), wm3.copy(),
+                                      self.mut_rate_wm1, self.mut_rate_wm2, self.mut_rate_wm3
+                                      ) for individual in range(0, self.pop_size)]
 
-        # initialize weights for SReLu activation function
-        self.wSRelu1 = None
-        self.wSRelu2 = None
-        self.wSRelu3 = None
+        # differentiate them by a mutation operation
+        for individual in range(0, self.pop_size):
+            self.population[individual].mutate()
 
         # create a SET-MLP model
         self.create_model()
@@ -131,68 +149,137 @@ class SET2_MLP_CIFAR10:
 
     def create_model(self):
 
-        # create a SET-MLP model for CIFAR10 with 3 hidden layers
-        self.model = Sequential()
-        self.model.add(Flatten(input_shape=(32, 32, 3)))
-        self.model.add(Dense(4000, name="sparse_1",kernel_constraint=MaskWeights(self.wm1),weights=self.w1))
-        self.model.add(SReLU(name="srelu1",weights=self.wSRelu1))
-        self.model.add(Dropout(0.3))
-        self.model.add(Dense(1000, name="sparse_2",kernel_constraint=MaskWeights(self.wm2),weights=self.w2))
-        self.model.add(SReLU(name="srelu2",weights=self.wSRelu2))
-        self.model.add(Dropout(0.3))
-        self.model.add(Dense(4000, name="sparse_3",kernel_constraint=MaskWeights(self.wm3),weights=self.w3))
-        self.model.add(SReLU(name="srelu3",weights=self.wSRelu3))
-        self.model.add(Dropout(0.3))
-        self.model.add(Dense(self.num_classes, name="dense_4",weights=self.w4)) #please note that there is no need for a sparse output layer as the number of classes is much smaller than the number of input hidden neurons
-        self.model.add(Activation('softmax'))
+        for individual in range(0, self.pop_size):
 
-    def rewireMask(self,weights, noWeights):
-        # rewire weight matrix
+            # create a SET-MLP model for CIFAR10 with 3 hidden layers
+            self.population[individual].model = Sequential()
+            self.population[individual].model.add(Flatten(input_shape=(32, 32, 3)))
+            self.population[individual].model.add(Dense(4000,
+                                                        name="sparse_1",  kernel_constraint=MaskWeights(self.population[individual].wm1),
+                                                        weights=self.population[individual].w1))
+            self.population[individual].model.add(SReLU(name="srelu1"))
+            self.population[individual].model.add(Dropout(0.3))
+            self.population[individual].model.add(Dense(1000,
+                                                        name="sparse_2", kernel_constraint=MaskWeights(self.population[individual].wm2),
+                                                        weights=self.population[individual].w2))
+            self.population[individual].model.add(SReLU(name="srelu2"))
+            self.population[individual].model.add(Dropout(0.3))
+            self.population[individual].model.add(Dense(4000,
+                                                        name="sparse_3",  kernel_constraint=MaskWeights(self.population[individual].wm3),
+                                                        weights=self.population[individual].w3))
+            self.population[individual].model.add(SReLU(name="srelu3"))
+            self.population[individual].model.add(Dropout(0.3))
+            self.population[individual].model.add(Dense(self.num_classes, name="dense_4",
+                                                        weights=self.population[individual].w4))
 
-        # remove zeta largest negative and smallest positive weights
-        values = np.sort(weights.ravel())
-        firstZeroPos = find_first_pos(values, 0)
-        lastZeroPos = find_last_pos(values, 0)
-        largestNegative = values[int((1-self.zeta) * firstZeroPos)]
-        smallestPositive = values[int(min(values.shape[0] - 1, lastZeroPos +self.zeta * (values.shape[0] - lastZeroPos)))]
-        rewiredWeights = weights.copy();
-        rewiredWeights[rewiredWeights > smallestPositive] = 1;
-        rewiredWeights[rewiredWeights < largestNegative] = 1;
-        rewiredWeights[rewiredWeights != 1] = 0;
-        weightMaskCore = rewiredWeights.copy()
+            #please note that there is no need for a sparse output layer as the number of classes is much smaller than the number of input hidden neurons
+            self.population[individual].model.add(Activation('softmax'))
 
-        # add zeta random weights
-        nrAdd = 0
-        noRewires = noWeights - np.sum(rewiredWeights)
-        while (nrAdd < noRewires):
-            i = np.random.randint(0, rewiredWeights.shape[0])
-            j = np.random.randint(0, rewiredWeights.shape[1])
-            if (rewiredWeights[i, j] == 0):
-                rewiredWeights[i, j] = 1
-                nrAdd += 1
+    def find_first_pos(self, array, value):
+        idx = (np.abs(array - value)).argmin()
+        return idx
 
-        return [rewiredWeights, weightMaskCore]
+    def find_last_pos(self, array, value):
+        idx = (np.abs(array - value))[::-1].argmin()
+        return array.shape[0] - idx
 
-    def weightsEvolution(self):
-        # this represents the core of the SET procedure. It removes the weights closest to zero in each layer and add new random weights
-        self.w1 = self.model.get_layer("sparse_1").get_weights()
-        self.w2 = self.model.get_layer("sparse_2").get_weights()
-        self.w3 = self.model.get_layer("sparse_3").get_weights()
-        self.w4 = self.model.get_layer("dense_4").get_weights()
+    def createWeightsMask(self, epsilon, noRows, noCols):
+        # generate an Erdos Renyi sparse weights mask
+        mask_weights = np.random.rand(noRows, noCols)
+        p = (epsilon * (noRows + noCols)) / (noRows * noCols)
+        prob = 1 - p  # normal tp have 8x connections
+        mask_weights[mask_weights < prob] = 0
+        mask_weights[mask_weights >= prob] = 1
+        noParameters = np.sum(mask_weights)
 
-        self.wSRelu1 = self.model.get_layer("srelu1").get_weights()
-        self.wSRelu2 = self.model.get_layer("srelu2").get_weights()
-        self.wSRelu3 = self.model.get_layer("srelu3").get_weights()
+        return [noParameters, mask_weights, p]
 
-        [self.wm1, self.wm1Core] = self.rewireMask(self.w1[0], self.noPar1)
-        [self.wm2, self.wm2Core] = self.rewireMask(self.w2[0], self.noPar2)
-        [self.wm3, self.wm3Core] = self.rewireMask(self.w3[0], self.noPar3)
+    def update_population(self):
 
-        self.w1[0] = self.w1[0] * self.wm1Core
-        self.w2[0] = self.w2[0] * self.wm2Core
-        self.w3[0] = self.w3[0] * self.wm3Core
+        for individual in range(0, self.pop_size):
+            self.population[individual].w1 = self.population[individual].model.get_layer("sparse_1").get_weights()
+            self.population[individual].w2 = self.population[individual].model.get_layer("sparse_2").get_weights()
+            self.population[individual].w3 = self.population[individual].model.get_layer("sparse_3").get_weights()
+            self.population[individual].w4 = self.population[individual].model.get_layer("dense_4").get_weights()
+
+    def tournament_selection(self, k=2):
+        """
+        Perform tournament selection and return best individual
+        :param k: amount of individuals to participate in tournament
+        """
+        best_individual = None
+        for _ in range(k):
+            individual_index = randint(0, len(self.population) - 1)
+            if (best_individual is None) \
+                    or (self.population[individual_index].fitness > self.population[best_individual].fitness):
+                best_individual = individual_index
+        return best_individual
+
+    def cross(self, w1, w2):
+
+        if w1 is not None and w2 is not None:
+            nlines = len(w1)
+            ncols = len(w1[0])
+            points = np.random.uniform(0, 1, nlines)
+            points = np.repeat(points, ncols)
+            points = points.reshape(nlines, ncols) * ncols
+            indices_aux = np.tile(np.arange(ncols), nlines).reshape(nlines, ncols)
+            w = np.where(indices_aux < points, w1, w2)
+
+        return w
+
+    def evolve(self, individuals_accuracies):
+        print('>>> Evolution step')
+
+        offspring = []
+
+        for individual in range(0, self.pop_size):
+            self.population[individual].fitness = individuals_accuracies[individual]
+
+        for individual in range(0, self.pop_size):
+            parent1 = self.tournament_selection()
+            parent2 = parent1
+            while parent2 == parent1:
+                parent2 = self.tournament_selection()
+            #print('new individual '+str(individual)+' has parent1 '+str(parent1)+' and parent2 '+str(parent2))
+
+            wm1 = self.cross(self.population[parent1].wm1,
+                             self.population[parent2].wm1)
+
+            wm2 = self.cross(self.population[parent1].wm2,
+                             self.population[parent2].wm2)
+
+            wm3 = self.cross(self.population[parent1].wm3,
+                             self.population[parent2].wm3)
+
+            w1 = copy.copy(self.population[parent1].w1)
+            w1[0] = self.cross(self.population[parent1].w1[0],
+                               self.population[parent2].w1[0])
+
+            w2 = copy.copy(self.population[parent1].w2)
+            w2[0] = self.cross(self.population[parent1].w2[0],
+                               self.population[parent2].w2[0])
+
+            w3 = copy.copy(self.population[parent1].w3)
+            w3[0] = self.cross(self.population[parent1].w3[0],
+                               self.population[parent2].w3[0])
+
+            w4 = copy.copy(self.population[parent1].w4)
+            w4[0] = self.cross(self.population[parent1].w4[0],
+                               self.population[parent2].w4[0])
+
+            new_individual = Individual(wm1, wm2, wm3,
+                                        self.mut_rate_wm1, self.mut_rate_wm2, self.mut_rate_wm3,
+                                        w1, w2, w3, w4
+                                        )
+            new_individual.mutate()
+            offspring.append(new_individual)
+
+        self.population = offspring
 
     def train(self):
+
+        individual = 0
 
         # read CIFAR10 data
         [x_train,x_test,y_train,y_test]=self.read_data()
@@ -211,30 +298,50 @@ class SET2_MLP_CIFAR10:
             vertical_flip=False)  # randomly flip images
         datagen.fit(x_train)
 
-        self.model.summary()
+        # shows only one, as the topology is always equal
+        self.population[0].model.summary()
 
         # training process in a for loop
-        self.accuracies_per_epoch=[]
-        for epoch in range(0,self.maxepoches):
 
-            sgd = optimizers.SGD(lr=self.learning_rate, momentum=self.momentum)
-            self.model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+        self.best_accuracies = []
+        self.all_accuracies = []
+        for epoch in range(0, self.maxepoches):
 
-            historytemp = self.model.fit_generator(datagen.flow(x_train, y_train,
-                                             batch_size=self.batch_size),
-                                steps_per_epoch=x_train.shape[0]//self.batch_size,
-                                epochs=epoch,
-                                validation_data=(x_test, y_test),
-                                 initial_epoch=epoch-1)
+            individuals_accuracies = []
+            for individual in range(0, self.pop_size):
 
-            self.accuracies_per_epoch.append(historytemp.history['val_acc'][0])
+                sgd = optimizers.SGD(lr=self.learning_rate, momentum=self.momentum)
+                self.population[individual].model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+                historytemp = self.population[individual].model.fit_generator(datagen.flow(x_train, y_train,
+                                                                                           batch_size=self.batch_size),
+                                                                              steps_per_epoch=x_train.shape[0]//self.batch_size,
+                                                                              epochs=epoch,
+                                                                              validation_data=(x_test, y_test),
+                                                                              initial_epoch=epoch-1)
+                individuals_accuracies.append(historytemp.history['val_acc'][0])
+                self.all_accuracies.append(historytemp.history['val_acc'][0])
+
+                print('Individual ' + str(individual))
+                print(' n. params of wm1 ' + str(np.sum(self.population[individual].wm1)))
+                print(' n. params of wm2 ' + str(np.sum(self.population[individual].wm2)))
+                print(' n. params of wm3 ' + str(np.sum(self.population[individual].wm3)))
+
+
+            self.best_accuracies.append(max(individuals_accuracies))
+            print('Best acc '+ str(round(self.best_accuracies[-1], 2)))
+
+            # evolves models after a number of epochs
+            if (epoch+1) % self.epochs_per_generation == 0:
+                self.update_population()
+                self.evolve(individuals_accuracies)
 
             #ugly hack to avoid tensorflow memory increase for multiple fit_generator calls. Theano shall work more nicely this but it is outdated in general
-            self.weightsEvolution()
             K.clear_session()
+
             self.create_model()
 
-        self.accuracies_per_epoch=np.asarray(self.accuracies_per_epoch)
+        self.best_accuracies = np.asarray(self.best_accuracies)
 
     def read_data(self):
 
@@ -263,22 +370,29 @@ class SET2_MLP_CIFAR10:
 if __name__ == '__main__':
 
 
+    now = datetime.datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M"))
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--sample_prop',
-                        default=1)
+                        default=0.02)
 
     parser.add_argument('--exp_name',
-                        default='base')
+                        default='new')
 
     config = parser.parse_args()
 
     # create and run a SET-MLP model on CIFAR10
     model=SET2_MLP_CIFAR10(config)
 
+    now = datetime.datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M"))
+
     # save accuracies over for all training epochs
     # in "results" folder you can find the output of running this file
-    np.savetxt("results_"+config.exp_name+"/set_mlp_srelu_sgd_cifar10_acc.txt", np.asarray(model.accuracies_per_epoch))
+    np.savetxt("results_"+config.exp_name+"/set2_mlp_srelu_sgd_cifar10_acc.txt", np.asarray(model.best_accuracies))
+    np.savetxt("results_" + config.exp_name + "/set2_mlp_srelu_sgd_cifar10_acc_all.txt", np.asarray(model.all_accuracies))
 
 
 
